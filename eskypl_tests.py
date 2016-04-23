a26977b12b3fb800a1a3c615f4c10b49bdf4c971
dankon <daniel.konwisorz@gmail.com>
2016-04-23 13:47:10 +0200

    Add main script file

---
+++ eskypl_tests.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""
+This is a simple script for tests esky.pl website functionality 
+by selenium/webdriver with python.
+"""
+
+if __name__ == "__main__":
+    print "START"
+    print "STOP"
