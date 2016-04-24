93104cd2a73e5a05604ae032aa0d48559426115d
dankon <daniel.konwisorz@gmail.com>
2016-04-24 14:02:40 +0200

    Adding task requirements for main test file

--- eskypl_tests.py
+++ eskypl_tests.py
@@ -2,8 +2,33 @@
 #!/usr/bin/env python
 """
 This is a simple script for tests esky.pl website functionality 
-by selenium/webdriver with python.
+by Selenium/Webdriver Python module.
 """
+from selenium import webdriver
+
+def main():
+    """
+    Main program for realization of esky.pl task 
+    with guidelines from eskypl_guidelines_in_polish.txt.
+    """
+    
+# 1.       Otworzenie strony
+
+# 2.       Pobranie i wyświetlenie w konsoli wartości ciastka esky_TCSI
+
+# 3.       Wypełnienie danych formularza wyszukiwania (parametryzowane destynacje oraz daty wylotu/powrotu 
+# wyliczane zawsze 3mce do przodu)
+
+# 4.       Wykonanie prostego filtrowania na wynikach wyszukiwania (dowolna kombinacja)
+
+# 5.       Pobieranie kilku istotnych elementów pojedynczego lotu
+
+# 6.       Przejście na kolejny ekran płatności i wypełnienie formularza dowolnymi wartościami 
+# spełniającymi reguły walidacji
+
+# 7.       Wykonanie kilku asercji, biorąc pod uwagę dane uzyskane na formularzu wyszukiwania, 
+# wynikach i ekranie płatności.
+    
 
 if __name__ == "__main__":
     print "START"
