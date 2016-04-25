400980f7d48e3e2e54665fab1fa07c33044c7a21
dankon <daniel.konwisorz@gmail.com>
2016-04-25 21:08:04 +0200

    Task #6 - finished  - fill up payments form and add some TODOs

--- eskypl_tests.py
+++ eskypl_tests.py
@@ -13,6 +13,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 MONTHS_PL = {1 : u"styczeń",   
              2 : u"luty",
@@ -89,16 +90,36 @@ def click_filter(filters_div, filter_grp_id, filter_id):
     filter.click()
 
     
-def fill_payments_form(payments_form, first_name, last_name, gender, phone_number, email_address):
+def fill_payments_form(payments_form, 
+                       first_name, 
+                       last_name, 
+                       gender, 
+                       phone_number, 
+                       email_address, 
+#                        birthday_date
+                       ):
     """
     Function for fill up payments form.
-    With first_name, last_name, gender, phone_number 
-     and email_address as params.
-    gender value "F" means female and "M" means male
+    Params: 
+    # first_name, 
+    # last_name, 
+    # gender - value "mrs" means female and "mr" means male, 
+    # phone_number, 
+    # email_address.
     """
-    _genders = ["F", "M"]
+#      birthday_date
+#     """
+    _genders = ["mrs", "mr"]
     if not gender in _genders:
         raise Exception('Bad gender value %s - please choose from %s' % (gender, _genders))
+    payments_form.find_element_by_id("bookFlight_paxes_1_name").send_keys(first_name)
+    payments_form.find_element_by_id("bookFlight_paxes_1_surname").send_keys(last_name)
+    gender_select = Select(payments_form.find_element_by_id("bookFlight_paxes_1_title"))
+    gender_select.select_by_value(gender)
+    payments_form.find_element_by_id("bookFlight_contactDetails_phoneNumber_phoneNumber").send_keys(phone_number)
+    payments_form.find_element_by_id("bookFlight_contactDetails_email").send_keys(email_address)
+    payments_form.find_element_by_id("bookFlight_statute").click()
+#     payments_form.find_element_by_id(
 
 
 def main():
@@ -147,28 +168,35 @@ def main():
                  filter_id = "filterDepartureTime0_opt_12_18") 
     print "Task #4: using filters on results website"
     # 5.       Pobieranie kilku istotnych elementów pojedynczego lotu
+    #TODO: 20160425-04 Task #5 take data from single flight
     available_flights_div = wait.until(
         expected_conditions.visibility_of_element_located((By.ID, "available-flights"))    
     )
     available_flights = available_flights_div.find_elements_by_tag_name("form")
     # 6.       Przejście na kolejny ekran płatności i wypełnienie formularza dowolnymi wartościami 
     # spełniającymi reguły walidacji
+    #TODO: 20160425-02 fix reserve flights issue - remove random? 
     sample(available_flights, 1)[0].find_element_by_xpath("./div[1]/a").click()
     payment_website_title = u"eSky: Hamburg - Katowice"
     wait.until(
         expected_conditions.title_is(payment_website_title)
     )
     assert esky_wd.title == payment_website_title
-    payments_form = None
+    payments_form = esky_wd.find_element_by_css_selector("form.booking-form")
+    #TODO: 20160425-01 fix birthday_date issue
     fill_payments_form(payments_form, 
                        first_name="John", 
                        last_name="Doe", 
-                       gender="M", 
+                       gender="mr", 
                        phone_number=789456123, 
-                       email_address="john.doe@hotmail.com")
+                       email_address="john.doe@hotmail.com",
+#                        birthday_date=date.today()+relativedelta(years=-20)
+                       )
+    payments_form.submit()
     print "Task #6 fill up payments form"
     # 7.       Wykonanie kilku asercji, biorąc pod uwagę dane uzyskane na formularzu wyszukiwania, 
     # wynikach i ekranie płatności.
+    #TODO: 20160425-03 Task #7 prepare more asserts
 
 #     esky_wd.close()
 
